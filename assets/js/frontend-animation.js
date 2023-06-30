79b294787a9d68466ed22b5409a060d09193ce21
Leonardo Souza <Leonardo23s.pt@gmail.com>
2023-06-30 12:09:52 +0100

    correção do about

--- assets/js/frontend-animation.js
+++ assets/js/frontend-animation.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.frontend-text', { 
-    strings: ['Frontend Developer', 'Frontend Developer'],
+    strings: ['Front-end Developer', 'Front-end Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true 
